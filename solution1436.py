cbf0f9ecbc799b772b1ade7adadc7a0b3a9dd09d
ramses2099 <ramses2099@gmail.com>
2024-05-11 18:41:21 -0400

    problem 1436 Destination city

---
+++ solution1436.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    """1436. Destination City"""
+    def destCity(self, paths: List[List[str]]) -> str:
+        outgoing_count = {}
+        for path in paths:
+            city_a, city_b = path
+            outgoing_count[city_a] = outgoing_count.get(city_a, 0) + 1
+            outgoing_count[city_b] = outgoing_count.get(city_b, 0)
+            
+        for city in outgoing_count:
+            if outgoing_count[city] == 0:
+                return city
+            
