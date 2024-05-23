bd39d3ff3304bb7570758095f5b76d1ae5743c57
ramses2099 <ramses2099@gmail.com>
2024-05-22 22:46:14 -0400

    last exercice

--- main.py
+++ main.py
@@ -51,15 +51,50 @@ def staircase(n):
     for j in range(n-1, 0, -1):
         print(" "*(n-j), end="")
         print("#"*j)
+
+def miniMaxSum(arr):
+    size_arr = len(arr)
+    i = 0
+    rs = []
+    
+    while(i <= size_arr-1):
+        t = 0 
+        for l in range(size_arr):            
+            if l == i:
+                continue                
+            t += arr[l]
         
+        i+=1
+        rs.append(t)
+    print(f"{min(rs)} {max(rs)}")    
+    
+    
+def miniMaxSum2(arr):
+    arr.sort()
+    size_arr = len(arr) -1
+    add=sum(arr)
+    print(f"{(add - arr[size_arr])} {(add-arr[0])}")            
+        
+def birthdayCakeCandles(candles):
+    m = max(candles)
+    count = 0
+    for i in range(len(candles)):
+        if m == candles[i]:
+            count += 1
+    return count
+
 
 def main():
     os.system("clear")
     # input
-    n = 10
-    staircase(n)
+    # arr = [1,2,3,4,5]
+    # arr = [7,69,2,221,8974]
     
-   
+    # miniMaxSum(arr)
+    # miniMaxSum2(arr)
+    arr = [3,2,1,3]
+    print(birthdayCakeCandles(arr))
+
     
 
 
